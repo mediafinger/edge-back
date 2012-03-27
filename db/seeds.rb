eb6850dd8739cb76ee9a7f23ba400cc5f76097a7
Andreas Finger <webmaster@mediafinger.com>
2012-03-28 00:28:22 +0200

    Make ready for first deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 events = [Fabricate(:event),
-          Fabricate(:event, :name => 'Rails UGHH'),
-          Fabricate(:event, :name => 'XP', :description => 'Xtreme programming'),
-          Fabricate(:event, :name => 'Limited WIP Society', :description => 'Kanban & Co'),
-          Fabricate(:event, :name => 'Limited WIP Society', :description => 'Kanban & Co', :city => 'Muenchen'),
-          Fabricate(:event, :name => 'Frozen Rails', :city => 'Helsinki', :country => 'fi', :description => 'Conference'),
-          Fabricate(:event, :name => 'Euruko', :city => 'Amsterdam', :country => 'nl',  :description => 'European Ruby Konferenz'),
-          Fabricate(:event, :name => 'Euruko', :city => 'Berlin', :description => 'European Ruby Konferenz'),
-          Fabricate(:event, :name => 'Arrrrcamp', :city => 'Ghent', :country => 'be',  :description => 'Aye'),
-          Fabricate(:event, :name => 'Scotland on Rails', :city => 'Edinburgh', :country => 'sc',  :description => 'Cheers'),
-          Fabricate(:event, :name => 'Scottish Ruby Conference', :city => 'Edinburgh', :country => 'sc', :description => 'Cheers')       
+          Fabricate(:event, :name => 'Rails UGHH', :city => 'Hamburg', :country => 'de', :start_date => 20120314),
+          Fabricate(:event, :name => 'XP', :description => 'Xtreme programming', :city => 'Hamburg', :country => 'de', :start_date => 20111218),
+          Fabricate(:event, :name => 'Limited WIP Society', :description => 'Kanban & Co', :city => 'Hamburg', :country => 'de', :start_date => 20120330),
+          Fabricate(:event, :name => 'Limited WIP Society', :description => 'Kanban & Co', :city => 'Muenchen',:country => 'de', :start_date => 20120326),
+          Fabricate(:event, :name => 'Frozen Rails', :city => 'Helsinki', :country => 'fi', :description => 'Conference', :start_date => 20110917),
+          Fabricate(:event, :name => 'Euruko', :city => 'Amsterdam', :country => 'nl',  :description => 'European Ruby Konferenz', :start_date => 20120601),
+          Fabricate(:event, :name => 'Euruko', :city => 'Berlin', :country => 'de', :description => 'European Ruby Konferenz', :start_date => 20110601),
+          Fabricate(:event, :name => 'Arrrrcamp', :city => 'Ghent', :country => 'be',  :description => 'Aye', :start_date => 20121004),
+          Fabricate(:event, :name => 'Scotland on Rails', :city => 'Edinburgh', :country => 'gb',  :description => 'Cheers', :start_date => 20110623),
+          Fabricate(:event, :name => 'Scottish Ruby Conference', :city => 'Edinburgh', :country => 'gb', :description => 'Cheers', :start_date => 20120628)       
          ]
